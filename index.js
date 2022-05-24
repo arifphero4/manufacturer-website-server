d9da8cd762cfc46a74ba5ac9a77c19db1bdadf16
arifphero <ahammadarif232@gmail.com>
2022-05-24 09:11:13 +0600

    tool api done

--- index.js
+++ index.js
@@ -10,7 +10,30 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elwai.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const toolCollection = client.db("manufacturer_admin").collection("tools");
+
+    //http://localhost:5000/tool
+    app.get("/tool", async (req, res) => {
+      const query = {};
+      const cursor = toolCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(" Hello World!Hello World!");

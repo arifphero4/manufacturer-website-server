d690b4b0d26ae3cdc3e575e8c481582ea6618dfb
arifphero <ahammadarif232@gmail.com>
2022-05-24 08:47:05 +0600

    connected to database

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.elwai.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(" Hello World!Hello World!");
 });
 
 app.listen(port, () => {

98615feed3c47d6bf08ca547fca342bccfd2a032
arifphero <ahammadarif232@gmail.com>
2022-05-26 23:05:03 +0600

    created make admin api

--- index.js
+++ index.js
@@ -64,6 +64,17 @@ async function run() {
       );
       res.send({ result, token });
     });
+
+    // user admin
+    app.put("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }

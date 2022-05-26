29cfa12bb8075100fbbc0387f8090668656c17c6
arifphero <ahammadarif232@gmail.com>
2022-05-26 23:42:06 +0600

    verify admin

--- index.js
+++ index.js
@@ -80,15 +80,31 @@ async function run() {
       res.send({ result, token });
     });
 
+    //check admin
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     // user admin
     app.put("/user/admin/:email", async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: "admin" },
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbiden" });
+      }
     });
   } finally {
     // await client.close();
